fb0d4167431a4a0e15b1a443fa604f34d9bd7c80
Parth <137251828+rabbitb8mile@users.noreply.github.com>
2023-06-27 09:00:34 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,65 @@
+#Aritmatic operator
+a=15
+b=5
+Addition=a+b
+print("addition is",Addition)
+Subtraction=a-b
+print("subtraction is",Subtraction)
+Multiplication=a*b
+print("multiplication is",Multiplication)
+Division=a/b
+print("division is",Division)
+Modulors=a%b
+print("modulors is",Modulors)
+Exponential=a**b
+print("exponential is ",Exponential)
+Floordivision=a//b
+print("floor division is ",Floordivision)
+
+#assignment operators
+a=20
+a+=5
+print("+=",a)
+a=20
+a-=5
+print("-=",a)
+a=20
+a*=5
+print("*=",a)
+
+#Comparison operators
+a=100
+b=50
+a>b
+print(">",a>b)
+a<b
+print("<",a<b)
+a==b
+print("==",a==b)
+a!=b
+print("!=",a!=b)
+
+#logical operators 
+a=True
+b=False
+print("and operation between a and b:",a and b)
+print("or operation between a and b:",a or  b)
+print("not  operation between a and b:", not b )
+
+#Bitwise operator 
+a=50
+b=10
+print("bitwise and operation between a and b:",a&b)
+print("bitwise or operation between a and b:",a|b)
+print("bitwise exponential operation between a and b:",a^b)
+
+#Identity operators 
+a=20
+b=5
+print("is",a is b)
+print("is not", a is not b)
+
+#membership operators 
+a="Parth"
+print('a'in a )
+print("y"not in a)
